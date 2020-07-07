b29e4f56861651289eac084ffda32a52ddc379b2
uronohan <uronohan@gmail.com>
2020-07-07 16:29:37 +0700

    adding router

---
+++ controllers/admin.js
@@ -0,0 +1,32 @@
+const db = require('../models');
+
+//user and setting management 
+
+exports.listAllUser = () => {}
+exports.createUser = (req, res, next) => {
+    const {
+        firstname,
+        lastname,
+        email,
+        phone,
+        login_name,
+        password,
+
+    }
+}
+exports.updateUser = () => {}
+exports.resetUserPassword = () => {}
+
+
+
+//company details 
+
+exports.createComDetails = () => {}
+exports.updateComDetails = () => {}
+exports.deleteComDetails = () => {}
+exports.deleteComData = () => {}
+
+//adding shipper
+exports.createShipper = () => {}
+exports.updateShipper = () => {}
+exports.deleteShipper = () => {}
