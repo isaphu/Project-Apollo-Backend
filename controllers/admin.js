33c9140cfe4123ed6ec60b16f3615d37a2da6515
uronohan <uronohan@gmail.com>
2020-07-07 16:53:47 +0700

    update admin controller

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,22 +1,33 @@
 const db = require('../models');
 const jwtDecode = require('jwt-decode');
+const { request } = require('express');
 //user and setting management 
 
 exports.listAllUser = () => {}
 exports.createUser = (req, res, next) => {
-    const {
-        firstname,
-        lastname,
-        email,
-        phone,
-        login_name,
-        password,
-        isAdmin,
-        register_complete,
-        status
-    } = req.body;
-
-
+    try {
+        const {
+            firstname,
+            lastname,
+            email,
+            phone,
+            login_name,
+            password,
+            isAdmin,
+            register_complete,
+            status
+        } = req.body;
+        db.user.findOne({where: { usernmae: jwtDecode(JSON.stringify(req.headers.authorization)).login_name}})
+        .then(requestUser =>{
+            if (requestUser.isAdmin) {
+                db.user
+                .create({ login_name, password: bcryptjs.hashSync(password, bcryptjs.genSaltSync(12)), firstname,lastname,email,phone,isAdmin,register_complete,status})
+                .then(result => res.status(200).send({ message: 'User Created!'}))
+            } else res.status(401).send({ message: 'Unauthorize Request!'})   
+        })
+    } catch(err) {
+        res.status(401).send({ message: 'Unauthorize Request!', err});
+    }
 }
 
 
