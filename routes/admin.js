4e2b2fd5b58a4e2fe41da792fd8d1b2e87e99fc8
uronohan <uronohan@gmail.com>
2020-07-07 16:30:11 +0700

    adding router

---
+++ routes/admin.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const adminController = require('../controllers/admin');
+
+//user and setting management 
+router.get('/', adminController.listAllUser)
+router.post('/', adminController.createUser)
+router.put('/', adminController.updateUser)
+router.post('./', adminController.resetUserPassword)
+router.delete('./', adminController.deleteUser)
+
+//company details
+router.post('/', adminController.createComDetails)
+router.put('./', adminController.updateComDetails)
+router.delete('./', adminController.deleteComDetails)
+router.delete('./', adminController.deleteComData)
+
+//adding shipper 
+router.post('/', adminController.createShipper)
+router.put('/', adminController.updateShipper)
+router.delete('./', adminController.deleteShipper)
