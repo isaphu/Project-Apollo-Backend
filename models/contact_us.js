c7b72207f3ff3e4eeab2aff6fb4756a51df2fa29
uronohan <uronohan@gmail.com>
2020-07-08 10:50:28 +0700

    update: adding LINE and EMAIL on contact_us models

--- models/contact_us.js
+++ models/contact_us.js
@@ -1,11 +1,17 @@
 module.exports = (sequelize, DataType) => {
     const contact_us = sequelize.define("contact_us", {
-        message: {
-            type: DataType.TEXT
+        line: {
+            type: DataType.STRING(255)
+        },
+        email: {
+            type: DataType.STRING(255)
+        },
+        createdAt: {
+            type: DataType.DATE
+        },
+        updatedAt: {
+            type: DataType.DATE
         }
     })
-    contact_us.associate = (models) => {
-        contact_us.belongsTo(models.user)
-    }
     return contact_us;
 }
