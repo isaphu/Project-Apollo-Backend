bda37c5239b07706b0e033694b3cea550a117d0c
Isa Phuyuthanon <uronohan@gmail.com>
2020-07-10 11:15:03 +0700

    update import_export route

--- routes/import_export.js
+++ routes/import_export.js
@@ -3,16 +3,22 @@ const router = express.Router();
 const import_exportController = require('../controllers/import_export');
 
 // import_entry
-router.get('/', import_exportController.listImportData)
+router.get('/', import_exportController.listAllImportData)
+router.get('/:id', import_exportController.listImportData)
+router.get('/', import_exportController.listAllImportProduct)
+router.get('/:id', import_exportController.listImportProduct)
 router.post('/', import_exportController.createImportData)
-router.put('/'.import_exportController.updateImportData)
+router.put('/', import_exportController.updateImportData)
 router.delete('/',import_exportController.deleteImportData)
 
 // export_entry
 router.get('/', import_exportController.listExportData)
+router.get('/:id', import_exportController.listExportData)
+router.get('/', import_exportController.listAllExportProduct)
+router.get('/:id', import_exportController.listExportProduct)
 router.post('/', import_exportController.createExportData)
-router.put('/'.import_exportController.updateExportData)
-router.delete('/',import_exportController.deleteExportData)
+router.put('/', import_exportController.updateExportData)
+router.delete('/', import_exportController.deleteExportData)
 
 
 module.exports = import_export;
