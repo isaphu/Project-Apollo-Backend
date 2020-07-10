78fbba1b667563b5e35886f5dc05a5247c97182d
Isa Phuyuthanon <uronohan@gmail.com>
2020-07-10 10:17:27 +0700

    adding import_export routes

--- routes/import_export.js
+++ routes/import_export.js
@@ -3,7 +3,16 @@ const router = express.Router();
 const import_exportController = require('../controllers/import_export');
 
 // import_entry
-router.get('/', import_exportController.get)
-
+router.get('/', import_exportController.listImportData)
+router.post('/', import_exportController.createImportData)
+router.put('/'.import_exportController.updateImportData)
+router.delete('/',import_exportController.deleteImportData)
 
 // export_entry
+router.get('/', import_exportController.listExportData)
+router.post('/', import_exportController.createExportData)
+router.put('/'.import_exportController.updateExportData)
+router.delete('/',import_exportController.deleteExportData)
+
+
+module.exports = import_export;
