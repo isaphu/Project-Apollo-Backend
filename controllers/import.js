a02e18a815c45d8057f112f104bdebf4413df4d6
Isa Phuyuthanon <uronohan@gmail.com>
2020-07-12 17:01:46 +0700

    fixing white space

--- controllers/import.js
+++ controllers/import.js
@@ -1,5 +1,6 @@
 const db = require('../models');
 const { response } = require('express');
+const { Op } = require('sequelize')
 
 exports.createImportData = async (req, res,next) => {
     try {
@@ -10,21 +11,21 @@ exports.createImportData = async (req, res,next) => {
             warehouse_date,
             releasing_no
         } = req.body;
-                if (!import_entry_number) {
+            if (!import_entry_number) {
                     return res.status(400).send({ message: 'กรุณากรอกเลขที่ใบขน'})
-                }
-                if (!arriving_date) {
+            }
+            if (!arriving_date) {
                     return res.status(400).send({ message: 'กรุณาเลือกวันเรือเข้า'})
-                }
-                if (!releasing_date) {
+            }
+            if (!releasing_date) {
                     return res.status(400).send({ message: 'กรูณาเลือกวันผ่านพิธีการ'})
-                }
-                if (!warehouse_date) {
+            }
+            if (!warehouse_date) {
                     return res.status(400).send({ message: 'กรุณาเลือกวันเข้าคลัง'})
-                }
-                if (!releasing_no) {
+            }
+            if (!releasing_no) {
                     return res.status(400).send({ message: 'กรุณากรอกเลขที่พิธีการ'})
-                }
+            }
         const newImportData = await db.import_entry.create ({
             import_entry: import_entry_number,
             arriving_date,
@@ -41,7 +42,13 @@ exports.createImportData = async (req, res,next) => {
 
 
 
-exports.getAllImportData = () => {};
+exports.getAllImportData = async (req, res, next) => {
+    try {
+
+    } catch(err) {
+        res.status(500).send({ message: err.message })
+    }
+};
 
 
 
--- controllers/product.js
+++ controllers/product.js
@@ -1,32 +1,32 @@
 const db = require('../models');
 const { response } = require('express');
-const { Op } =require('sequelize');
+const { Op } = require('sequelize');
 
 exports.createProduct = async (req, res, next) => {
     try {
-     const {
-         code,
-         name,
-         details,
-         uomImportId,
-         uomExportId
+        const {
+            code,
+            name,
+            details,
+            uomImportId,
+            uomExportId
         } = req.body;
-        if (!code) {
-            return res.status(400).send({ message: 'กรุณากรอกรหัสสินค้า'})
-        }
-        if (!name) {
-            return res.status(400).send({ message: 'กรุณากรอกชื่อสินค้า'})
-        } 
-        if (!details) {
-            return res.status(400).send({ message: 'กรุณากรอกรายละเอียดของสินค้า'})
-        }
+            if (!code) {
+                return res.status(400).send({ message: 'กรุณากรอกรหัสสินค้า'})
+            }
+            if (!name) {
+                return res.status(400).send({ message: 'กรุณากรอกชื่อสินค้า'})
+            } 
+            if (!details) {
+                return res.status(400).send({ message: 'กรุณากรอกรายละเอียดของสินค้า'})
+            }
      const newProduct = await db.product.create({
-        product_code: code,
-        product_name: name,
-        product_detail: details,
-        status: true,
-        uomExportId,
-        uomImportId
+            product_code: code,
+            product_name: name,
+            product_detail: details,
+            status: true,
+            uomExportId,
+            uomImportId
      })
       res.status(201).send({ message: 'สินค้าได้ถูกเพิ่มแล้ว'}) 
     } catch(err) {
