abceadbc14f9d277b21fb535ed753a3392e3e478
Isa Phuyuthanon <uronohan@gmail.com>
2020-07-28 10:52:38 +0700

    adding seeder

--- seeders/20200716230851-uom.js
+++ seeders/20200716230851-uom.js
@@ -1,11 +1,27 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-      return queryInterface.bulkInsert('uoms',[{
-        uom_code: 'KG',
-        uom_name: 'Kilogram',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }])
+      return queryInterface.bulkInsert('uoms',[
+        {
+          uom_code: 'KG',
+          uom_name: 'Kilogram',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          uom_code: 'GR',
+          uom_name: 'Gram',
+          createdAt: new Date(),
+          updatedAt: new Date()
+
+        },
+        {
+          uom_code: 'CR',
+          uom_name: 'Carton',
+          createdAt: new Date(),
+          updatedAt: new Date()
+
+        }
+      ])
   },
 
   down: async (queryInterface, Sequelize) => {
